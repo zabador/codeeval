36a5f528a8820cf03fbb587b6cd0535917b8e255
Skye <snschnei@oakland.edu>
2013-02-07 10:49:55 -0500

    added comments to fizzbuzz

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -22,26 +22,29 @@ public class fizzbuzz{
             while(line != null){
                 int index = 0;
                 for (String num : line.split(" ")){
-                    nums[index] = Integer.parseInt(num);
+                    nums[index] = Integer.parseInt(num);// set A B and max
                     index++;
                 }
 
                 s = "";
 
                 for(int i=1; i<=nums[2]; i++){
+                    // add F to s if divisible by A
                     if(i%nums[0] == 0){
-                        if(i==1)
+                        if(i==1)// put a space in front of character unless it is 1
                             s += "F";
                         else
                             s += " F";
                         if(i%nums[1] == 0)
                                 s += "B";
                    
+                        // add B to s  if divisible by B
                     }else if(i%nums[1] == 0)
                             if(i==1)
                                 s += "B";
                             else
                                 s += " B";
+                    // just print number if it is not a factor of A or B
                     else
                         if(i==1)
                             s += i;
