cae18be1f0c0e3a65eddb5f0b16521e501bff9df
Skye <snschnei@oakland.edu>
2013-02-14 11:50:04 -0500

    added files for previous commit

---
+++ src/position.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.lang.Integer;
+
+public class position{
+
+    private static ArrayList<String> input = new ArrayList<String>(); 
+
+    public static void main(String[] args){
+
+        
+        openFile(args[0]);
+        int[] decimals = new int[3];
+
+        for(int i=0; i<input.size(); i++){
+
+            int index = 0;
+            for(String a : input.get(i).split(",")){
+
+                decimals[index] = Integer.parseInt(a);
+                index++;
+            }
+
+
+            String binary = Integer.toBinaryString(decimals[0]);
+            System.out.println(binary.charAt(binary.length()-decimals[1]) == (binary.charAt(binary.length()-decimals[2])));
+        
+        }
+        System.exit(0);
+
+    }
+
+    
+    public static void openFile(String path){
+
+        try{
+            File file = new File(path);
+            BufferedReader in = new BufferedReader(new FileReader(file));
+
+            String s;
+            while((s = in.readLine()) != null){
+                if(s.trim().length()>0)
+                    input.add(s);
+            }
+            in.close();
+        }catch(IOException ioe){
+            System.out.println("File Read Error: " + ioe.getMessage());
+        }
+        
+    }
+}
