2ac9770209883afa9d467a02e21047e620cefbd4
zabador <zabador@beverly.(none)>
2013-10-07 13:05:15 -0400

    Easy sum_of_integers_from_file

---
+++ java/easy/src/sum_integers_from_file/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Main{
+
+    private static ArrayList<String> input = new ArrayList<String>(); 
+
+    public static void main(String[] args){
+
+        
+        openFile(args[0]);
+
+		int sum = 0;
+        for(int i=0; i<input.size(); i++){
+
+            int index = 0;
+         //   for(String a : input.get(i).split(",")){
+
+         //       // add words here
+         //       index++;
+         //   }
+            // logic goes here
+		sum += Integer.parseInt(input.get(i));
+			
+           
+        }
+		System.out.println(sum);
+        System.exit(0);
+
+    }
+
+    
+    public static void openFile(String path){
+
+        try{
+            File file = new File(path);
+            BufferedReader in = new BufferedReader(new FileReader(file));
+
+            String s;
+            while((s = in.readLine()) != null){
+                if(s.trim().length()>0)
+                    input.add(s);
+            }
+            in.close();
+        }catch(IOException ioe){
+            System.out.println("File Read Error: " + ioe.getMessage());
+        }
+        
+    }
+}
