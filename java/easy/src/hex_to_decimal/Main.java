5419385655b0ef74c36310fc1069cd9f131f9ce1
zabador <zabador2000@gmail.com>
2013-10-08 13:23:27 -0400

    easy hex to decimal

---
+++ java/easy/src/hex_to_decimal/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static ArrayList<String> input = new ArrayList<String>(); 
+
+    public static void main(String[] args){
+
+        
+        openFile(args[0]);
+
+        for(int i=0; i<input.size(); i++){
+
+
+			System.out.println(Integer.parseInt(input.get(i), 16));
+
+           
+        }
+        System.exit(0);
+
+    }
+
+    
+    public static void openFile(String path){
+
+        try{
+            File file = new File(path);
+            BufferedReader in = new BufferedReader(new FileReader(file));
+
+            String s;
+            while((s = in.readLine()) != null){
+                if(s.trim().length()>0)
+                    input.add(s);
+            }
+            in.close();
+        }catch(IOException ioe){
+            System.out.println("File Read Error: " + ioe.getMessage());
+        }
+        
+    }
+}
